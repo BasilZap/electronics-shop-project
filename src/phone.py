cba189d0b613fbddd4628c92d63b25ac55be2b9f
BasilZap <zapr9@yandex.ru>
2023-06-11 16:31:15 +0300

    Добавлены комментарии к методам, скорректирован метод add для класса Item

--- src/phone.py
+++ src/phone.py
@@ -1,11 +1,18 @@
 from src.item import Item
 
+
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
+        """
+        Инициализация дочернего класса Phone
+        """
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
     def __repr__(self):
+        """
+        Переопределение метода repr для класса Phone
+        """
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
 
